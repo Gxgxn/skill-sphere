4b48aa89f1a37b840a7ee4e1d9cb2006f39ac829
gxgxn <gdsinghb99@gmail.com>
2023-09-28 13:22:25 -0300

    Image Upload  & Course Category Form Added

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -37,7 +37,8 @@ const CreatePage = () => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const response = await axios.post("/api/courses", values);
-      router.push(`/teachers/courses/${response.data.id}`);
+      router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course Created");
     } catch (error) {
       toast.error("Something Went Wrong");
     }
