1970f4215877e0b9e4ad26bc39d17ca0f67b9982
gxgxn <gdsinghb99@gmail.com>
2023-09-29 00:58:36 -0300

    Course Layout Created

--- components/CoursesList.tsx
+++ components/CoursesList.tsx
@@ -1,4 +1,6 @@
 import { Category, Course } from "@prisma/client";
+import { CourseCard } from "./CourseCard";
+
 type CourseWithProgressWithCategory = Course & {
   category: Category | null;
   chapters: { id: string }[];
@@ -8,10 +10,29 @@ type CourseWithProgressWithCategory = Course & {
 interface CoursesListProps {
   items: CourseWithProgressWithCategory[];
 }
-interface CoursesListProps {
-  items: CourseWithProgressWithCategory[];
-}
 
 export const CoursesList = ({ items }: CoursesListProps) => {
-  return <></>;
+  return (
+    <div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl!}
+            chaptersLength={item.chapters.length}
+            price={item.price!}
+            progress={item.progress}
+            category={item?.category?.name!}
+          />
+        ))}
+      </div>
+      {items.length === 0 && (
+        <div className="text-center test-sm text-muted-foreground mt-10">
+          No Courses Found
+        </div>
+      )}
+    </div>
+  );
 };
